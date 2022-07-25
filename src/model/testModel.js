e40109ba7447b8291146d5d7efe109bbf4ba31f8
joonhyukchoi <cjh4787@gmail.com>
2022-07-25 17:35:20 +0900

    feat: 소켓 처음 연결할 때 유저 정보 받는 부분 추가

--- src/model/testModel.js
+++ src/model/testModel.js
@@ -1,35 +1,51 @@
 // const { pool } = require("../../config/database");
 // const mongoose = require('mongoose');
 const Client = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/test'
-
-function dbCreate(id, value) {
-    Client.connect(url, function(err, database) {
-      if (err) throw err;
-      dbFind(value);
-      database.close();
-    })
-  }
+var url = 'mongodb://localhost:27017/editors'
   
- function dbFind(value) {
-    Client.connect(url, async function(err, database) {
-      if (err) throw err;
-      let db = database.db('editors');
-      let result = db.collection('editor').findOne({id: value.id});
-      return Promise.all([result])
-      .then(function(values){
-        if(values[0]) {
-          dbUpdate(value.id, value.pdfId, value.text);
-          console.log('database updated');
-          database.close();
-        } else {
-          dbInsert(value.id, value.pdfId, value.text);
-          console.log('database inserted');
-          database.close();
-        }
-      })
+function dbFind(value) {
+  Client.connect(url, async function(err, database) {
+    if (err) throw err;
+    let db = database.db('editors');
+    let result = db.collection('editor').findOne({id: value.id, pdfId: value.pdfId});
+    return Promise.all([result])
+    .then(function(values){
+      if(values[0]) {
+        dbUpdate(value.id, value.pdfId, value.text);
+        console.log('database updated');
+        database.close();
+      } else {
+        dbInsert(value.id, value.pdfId, value.text);
+        console.log('database inserted');
+        database.close();
+      }
     })
-  }
+  })
+}
+
+function dbUpdate(id, pdfId, text) {
+  Client.connect(url, async function(err, database) {
+    if (err) throw err;
+    let db = database.db('editors');
+    let result = db.collection('editor').updateOne({id: id, pdfId: pdfId}, {$set: {text: text}});
+    return result;
+  })
+}
+
+function dbInsert(id, pdfId, text) {
+  Client.connect(url, async function(err, database) {
+    if (err) throw err;
+    let db = database.db('editors');
+    result = db.collection('editor').insertOne({id: id, pdfId: pdfId, text: text});
+    return result;
+  })
+}
+
+module.exports = {
+    dbFind,
+}
+
+// async await 적용 실패사례
 
   // function dbSearch(id, pdfId) {
   //   Client.connect(url, async function(err, database) {
@@ -94,26 +110,3 @@ function dbCreate(id, value) {
 //     return new Promise(result);
 //   })
 // }
-
-function dbUpdate(id, pdfId, text) {
-  Client.connect(url, async function(err, database) {
-    if (err) throw err;
-    let db = database.db('editors');
-    let result = db.collection('editor').updateOne({id: id, pdfId: pdfId}, {$set: {text: text}});
-    return result;
-  })
-}
-
-function dbInsert(id, pdfId, text) {
-  Client.connect(url, async function(err, database) {
-    if (err) throw err;
-    let db = database.db('editors');
-    result = db.collection('editor').insertOne({id: id, pdfId: pdfId, text: text});
-    return result;
-  })
-}
-
-module.exports = {
-    dbCreate,
-    dbFind,
-}
