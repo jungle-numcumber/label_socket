591064dcb9b48deaf6b623a058aa3fb819343b47
joonhyukchoi <cjh4787@gmail.com>
2022-07-31 20:23:36 +0900

    feat: find bug2

--- index.js
+++ index.js
@@ -20,52 +20,52 @@ try {
     cert: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/' + domain+ '/cert.pem'), 'utf8').toString(),
   };
   const server = HTTPS.createServer(option, app);
-  const io = require('socket.io')(server, {});
-  app.get('/test', (req, res) => {
-    res.send('hello test');
-  // 미들웨어 압축, 파일 용량 줄임
-  app.use(compression());
-  app.use(express.json());
-  // restful api 중 put, delete를 사용하기 위해 씀
-  app.use(methodOverride());
-  // urlencoded 페이로드로 들어오는 요청을 분석, extended true는 qs 모듈을 써서 body parsing
-  app.use(express.urlencoded({extended: true}));
-  // 모든 도메인에서 나의 서버에게 요청을 보낼 수 있게 해줌
-  app.use(cors());
-  // editor room websocket connection
-  // 클라이언트로부터 connection 이벤트를 받는다. 
-  // ** handshake가 완료되면 emitted 된다.
-  io.on('connection', async (socket) => {
-    console.log('user connected');
-    
-    // let result = await dbSearch(socket.request._query.userId, socket.request._query.pdfId);
-    let defaultPage = ""
-    if (result !== null) {
-      console.log('result?',result['text'])
-      defaultPage = result['text'];
-    }
-
-    io.emit('updateEditorOnce', defaultPage, ()=>{
-      socket.on('updateEditor', (value) => {
-        // dbFind(value);
-        console.log(value.text);
-      });
-    });
-
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-  });
-  })
-
-  server.listen(sslport, () => {
-    console.log('[HTTPS] Server is started on port 443');
-  });
 
 } catch (error) {
   console.log('[HTTPS] Server is not Active. Please Check Your Server');
   console.log(error);
 }
+
+const io = require('socket.io')(server, {});
+app.get('/test', (req, res) => {
+  res.send('hello test');
+// 미들웨어 압축, 파일 용량 줄임
+app.use(compression());
+app.use(express.json());
+// restful api 중 put, delete를 사용하기 위해 씀
+app.use(methodOverride());
+// urlencoded 페이로드로 들어오는 요청을 분석, extended true는 qs 모듈을 써서 body parsing
+app.use(express.urlencoded({extended: true}));
+// 모든 도메인에서 나의 서버에게 요청을 보낼 수 있게 해줌
+app.use(cors());
+// editor room websocket connection
+// 클라이언트로부터 connection 이벤트를 받는다. 
+// ** handshake가 완료되면 emitted 된다.
+io.on('connection', async (socket) => {
+  console.log('user connected');
+  // let result = await dbSearch(socket.request._query.userId, socket.request._query.pdfId);
+  // let defaultPage = ""
+  // if (result !== null) {
+  //   console.log('result?',result['text'])
+  //   defaultPage = result['text'];
+  // }
+
+  // io.emit('updateEditorOnce', defaultPage, ()=>{
+  //   socket.on('updateEditor', (value) => {
+  //     dbFind(value);
+  //     console.log(value.text);
+  //   });
+  // });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+})
+
+  server.listen(sslport, () => {
+    console.log('[HTTPS] Server is started on port 443');
+  });
 
 
 // // #!/usr/bin/env node
